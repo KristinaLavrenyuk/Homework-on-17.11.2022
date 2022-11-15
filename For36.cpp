95b9ad0644b917431aa27f76721fa0e77ac43f58
Kristina Lavrenyuk <115408741+KristinaLavrenyuk@users.noreply.github.com>
2022-11-15 20:26:13 +0300

    Add files via upload

---
+++ For36.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double n, k, sum = 0, slag;
+    cin >> n >> k;
+
+    for (int i = 1; i <= n; i++)
+    {
+        slag = i;
+        for (int j = 1; j < k; j++)
+        {
+            slag *= i;
+        }
+        sum += slag;
+    }
+    cout << fixed << setprecision(2) << sum << endl;
+    system("pause");
+}
